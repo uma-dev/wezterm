9017165f5c6c5e57c57f2b71d4d51bb0cec94c73
Omar Roldán <omar.roldan.50@gmail.com>
2024-08-12 16:26:04 -0600

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,8 +1,7 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
+local mux = wezterm.mux
 local act = wezterm.action
-
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
 -- FONT
@@ -34,8 +33,8 @@ config.color_scheme = "Tokyo Night (Gogh)"
 
 -- TAB BAR
 config.use_fancy_tab_bar = false
-config.hide_tab_bar_if_only_one_tab = true
-config.status_update_interval = 1000
+-- config.hide_tab_bar_if_only_one_tab = true
+config.status_update_interval = 300
 config.tab_bar_at_bottom = true
 config.tab_max_width = 12
 config.colors = {
@@ -73,6 +72,58 @@ config.colors = {
 	-- recolor cursor when leader active
 	compose_cursor = "orange",
 }
+wezterm.on("update-status", function(window, pane)
+	-- Workspace name
+	local stat = window:active_workspace()
+	local stat_color = "#9d9df7"
+	local first_color = "#c0c0c0"
+	-- It's a little silly to have workspace name all the time
+	-- Utilize this to display LDR or current key table name
+	if window:active_key_table() then
+		stat = window:active_key_table()
+		stat_color = "#7dcfff"
+	end
+	if window:leader_is_active() then
+		-- stat = "LDR"
+		stat_color = "orange"
+	end
+
+	local basename = function(s)
+		-- Nothing a little regex can't fix
+		return string.gsub(s, "(.*[/\\])(.*)", "%2")
+	end
+
+	-- Current working directory
+	local cwd = pane:get_current_working_dir()
+	if cwd then
+		if type(cwd) == "userdata" then
+			-- Wezterm introduced the URL object in 20240127-113634-bbcac864
+			cwd = basename(cwd.file_path)
+		else
+			-- 20230712-072601-f4abf8fd or earlier version
+			cwd = basename(cwd)
+		end
+	else
+		cwd = ""
+	end
+
+	-- Current command
+	local cmd = pane:get_foreground_process_name()
+	-- CWD and CMD could be nil (e.g. viewing log using Ctrl-Alt-l)
+	cmd = cmd and basename(cmd) or ""
+
+	-- Right status
+	window:set_right_status(wezterm.format({
+		{ Foreground = { Color = first_color } },
+		{ Text = wezterm.nerdfonts.md_folder .. "  " .. cwd },
+		{ Text = " | " },
+		{ Text = wezterm.nerdfonts.fa_code .. "  " .. cmd },
+		{ Text = " | " },
+		{ Foreground = { Color = stat_color } },
+		{ Text = wezterm.nerdfonts.oct_table .. "  " .. stat },
+		{ Text = " " },
+	}))
+end)
 
 -- WINDOW
 -- config.window_background_opacity = 0.92
@@ -81,18 +132,10 @@ config.colors = {
 -- config.window_decorations = "RESIZE | TITLE"
 config.window_decorations = "RESIZE"
 config.window_close_confirmation = "AlwaysPrompt"
-config.initial_rows = 35
+config.initial_rows = 55
+config.initial_cols = 100
 
 -- KEYS
-config.keys = {
-	{
-		key = "w",
-		mods = "CMD",
-		action = wezterm.action.CloseCurrentTab({ confirm = true }),
-	},
-}
-
--- Keys
 config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
 	-- Send C-a when pressing C-a twice
@@ -114,6 +157,26 @@ config.keys = {
 		mods = "LEADER",
 		action = act.ActivateKeyTable({ name = "resize_pane", one_shot = false }),
 	},
+	-- Tab keybindings
+	-- { key = "[", mods = "LEADER", action = act.ActivateTabRelative(-1) },
+	-- { key = "]", mods = "LEADER", action = act.ActivateTabRelative(1) },
+	-- { key = "n", mods = "LEADER", action = act.ShowTabNavigator },
+	{
+		key = "e",
+		mods = "LEADER",
+		action = act.PromptInputLine({
+			description = wezterm.format({
+				{ Attribute = { Intensity = "Bold" } },
+				{ Foreground = { AnsiColor = "Fuchsia" } },
+				{ Text = "New Tab Title: " },
+			}),
+			action = wezterm.action_callback(function(window, pane, line)
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
 }
 
 config.key_tables = {
@@ -126,15 +189,6 @@ config.key_tables = {
 		{ key = "Enter", action = "PopKeyTable" },
 	},
 }
-
--- Update right status to indicate leader key activation
-wezterm.on("update-right-status", function(window, pane)
-	local leader = ""
-	if window:leader_is_active() then
-		leader = "LEADER"
-	end
-	window:set_right_status(leader)
-end)
 
 -- config to skip no process when confirm
 config.skip_close_confirmation_for_processes_named = {}
